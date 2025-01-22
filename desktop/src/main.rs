f907cd1dcaf99f02dab85d5161176b8db6bfa56c
Teshan Liyanage <teshan.shanuka@cm-robotics.com>
2025-01-22 23:18:50 +0100

    feat: initial frontend

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,3 +1,78 @@
+use chip8::*;
+use sdl2::event::Event;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
+const SCALE: u32 = 15;
+const WINDOW_WIDTH: u32 = (SCREEN_W as u32) * SCALE;
+const WINDOW_HEIGHT: u32 = (SCREEN_H as u32) * SCALE;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: cargo run path/to/game");
+        return;
+    }
+
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window("Chip-8 Emulator", WINDOW_WIDTH, WINDOW_HEIGHT)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+    canvas.clear();
+    canvas.present();
+
+    let mut chip8 = Emu::new();
+
+    let mut rom = File::open(&args[1]).expect("Unable to open file");
+    let mut buffer: Vec<u8> = Vec::new();
+
+    rom.read_to_end(&mut buffer).unwrap();
+    chip8.load(&buffer);
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    'gameloop: loop {
+        for evt in event_pump.poll_iter() {
+            match evt {
+                Event::Quit { .. } => {
+                    break 'gameloop;
+                }
+                _ => (),
+            }
+        }
+
+        chip8.tick();
+        draw_screen(&chip8, &mut canvas);
+    }
+}
+
+fn draw_screen(emu: &Emu, canvas: &mut Canvas<Window>) {
+    canvas.set_draw_color(Color::RGB(0, 0, 0)); // fill black
+    canvas.clear();
+
+    let screenbuf = emu.get_display();
+    // draw white pixels
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+
+    for (i, pixel) in screenbuf.iter().enumerate() {
+        if *pixel {
+            let x = (i % SCREEN_W) as u32;
+            let y = (i / SCREEN_W) as u32;
+
+            // draw a rectangle scaled
+            let rect = Rect::new((x * SCALE) as i32, (y * SCALE) as i32, SCALE, SCALE);
+            canvas.fill_rect(rect).unwrap();
+        }
+    }
+
+    canvas.present();
 }
