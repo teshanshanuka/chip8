ff79a7ffea6a27ce631523a240ee5e51a200fd62
Teshan Liyanage <teshan.shanuka@cm-robotics.com>
2025-01-23 14:07:18 +0100

    chore: deps and boilerplate for frontend

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,14 +1,15 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use chip8::*;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub struct EmuWasm {
+    chip8: Emu,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[wasm_bindgen]
+impl EmuWasm {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> EmuWasm {
+        EmuWasm { chip8: Emu::new() }
     }
 }
